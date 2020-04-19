e2c43196ebd93e1f4f93568aa0cbde9312cc87bb
anshulkmr01 <anshulkmr01@gmail.com>
2020-04-19 18:14:46 +0530

    config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://localhost/japayag';
-$config['base_url'] = 'http://japayag.com/';
+$config['base_url'] = 'https://japayag.com/';
 
 /*
 |--------------------------------------------------------------------------
